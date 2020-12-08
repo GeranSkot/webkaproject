4da2e93e5f1b4e915b4f2505ab637d72ec76ba15
Billy <example@gmail.com>
2020-12-08 20:48:47 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log('arr' + ' - object');
+
+let incr = 10;
+let decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(incr === decr);
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && isClose);
+
+
+
+
+
