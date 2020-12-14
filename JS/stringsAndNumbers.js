8cfc50c46f5082aae594a6d12d63e9715fec1f3c
Billy <example@gmail.com>
2020-12-14 21:24:13 +0300

    Added stringsAndNumbers.js

---
+++ JS/stringsAndNumbers.js
@@ -0,0 +1,28 @@
+
+const str = 'test';
+const arr = [1, 2, 4];
+
+console.log(str.length);
+console.log(arr.length);
+
+console.log(str.toUpperCase());
+
+const fruit = 'some fruit0';
+
+console.log(fruit.indexOf('fruit0'));
+
+const logg = 'hello world';
+
+console.log(logg.slice(6,11));
+console.log(logg.slice(6));
+
+console.log(logg.substring(6,11));
+
+console.log(logg.substr(6,11));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
