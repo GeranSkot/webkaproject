2956cc95001ba09c7af34e7023bf72f376bc6bc3
Billy <example@gmail.com>
2020-12-24 19:58:07 +0300

    26

---
+++ JS/25.js
@@ -0,0 +1,55 @@
+//to String
+
+// 1)
+
+console.log(typeof (String(null)));
+console.log(typeof (String(4)));
+
+
+// 2)
+
+console.log(typeof (5 + ''));
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+
+// to Number
+
+// 1)
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(typeof(+'4'));
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+// const answer = +prompt("Hello my slave","");
+
+// to boolean
+
+// 0, '', null, undefined, NaN; always false
+
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+//2)
+
+console.log(typeof(Boolean('4')));
+
+//3)
+
+console.log(typeof (!!"44444"));
---
+++ JS/26.js
@@ -0,0 +1,32 @@
+
+
+//let x = 5; alert(x++);
+
+// console.log([] + false - null + true); // NaN
+//
+// let y = 1;
+// let x=y=2;
+// console.log(x); //2
+
+// console.log([] + 1 + 2); // 12 (String)
+
+// console.log("1"[0]); // 1
+
+// console.log(2 && 1 && null && 0 && undefined); //null
+//And stopped on false
+
+// console.log(!!(a && b) === (1 && 2)); // false
+
+// console.log(null || 2 && 3 || 4); // 3
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+//
+// console.log(a == b); //false
+
+// console.log(+"Infinity"); //Infinity (number)
+
+// console.log ("Ёжик" > "яблоко"); //false
+
+// console.log(0 || "" || 2 || undefined || true  || false); //2
+
